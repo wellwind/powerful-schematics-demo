bed1ba3d8c36d717b6d735cfcd6930c1900fd20c
黃升煌 Mike <mike@miniasp.com>
2018-12-19 22:36:33 +0800

    調整路由設定

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { DemoFormComponent } from './demo-form/demo-form.component';
+import { DemoTableComponent } from './demo-table/demo-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DemoTableComponent,
+  },
+  {
+    path: 'form',
+    component: DemoFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
